49ff76562d6bfd16fe31e3e346e3daeb02428d6d
Elodie Royant <elodie.royant@lexiko.io>
2022-01-10 19:30:11 +0100

    saving todos

--- Assets/Scripts/Logging.cs
+++ Assets/Scripts/Logging.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,60 @@ using Serilog.Sinks.File;
 using Serilog.Formatting.Json;
 
 
+//TODO replace by serialog format message (time stamp etc)
+// degager serialog
+public class CustomError
+{
+    public string message;
+    public string stackTrace;
+    public DateTime timestamp;
+
+}
+
 public class Logging : MonoBehaviour
 {
-    void Awake()
-    {
-        var log = new LoggerConfiguration()
-            .WriteTo
-            .File(new JsonFormatter(), Application.dataPath + "/AppLog.json")
-            .CreateLogger();
+    private CustomError current;
+    public List<CustomError> batchErrors = new List<CustomError>();
+    private Serilog.ILogger logToJsonFile;
+    //private Serilog.ILogger logToWebService;
 
-        Log.Logger = log;
+    private void Awake()
+    {
+        logToJsonFile = new LoggerConfiguration()
+            .WriteTo
+            .File(
+                new JsonFormatter(renderMessage: true),
+                Application.dataPath + "/AppLog.json"
+            )
+            .CreateLogger();
+        Log.Logger = logToJsonFile;
+    }
+
+    void OnEnable()
+    {
+        Application.logMessageReceivedThreaded += HandleLog;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceivedThreaded -= HandleLog;
+        // TODO: send batch batchErrors to rabbitMQ
+        // if fail
+        // log to file
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        current = new CustomError { message = logString, stackTrace = stackTrace, timestamp = DateTime.Now };
+        // TODO: add different colors depending on logtype
+        // TODO: do not log in prod env
+        Debug.Log(current.message);
+
+        if (type == LogType.Error || type == LogType.Exception)
+        {
+            batchErrors.Add(current);
+        }
+
     }
 
 }
--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -8,8 +8,7 @@ public class MainCamera : MonoBehaviour
     void Start()
     {
         Screen.orientation = ScreenOrientation.Portrait;
-        Log.Information("Dividing {A} by {B}", 1.2f, 2.0f);
-        Debug.Log("Dividing {A} by {B}");
-
+        //Log.Information("Dividing {A} by {B}", 1.2f, 2.0f);
+        throw new System.Exception("Main Camera Starts");
     }
 }
