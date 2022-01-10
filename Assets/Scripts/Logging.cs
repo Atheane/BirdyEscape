cbba1c9db2be5e8c905f8da93398123d7552bbce
Elodie Royant <elodie.royant@lexiko.io>
2022-01-10 15:57:07 +0100

    format log files in json

--- Assets/Scripts/Logging.cs
+++ Assets/Scripts/Logging.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using Serilog;
 using Serilog.Sinks.File;
+using Serilog.Formatting.Json;
+
 
 public class Logging : MonoBehaviour
 {
     void Awake()
     {
-        var log = new LoggerConfiguration().WriteTo.File(Application.dataPath + "/AppLog.txt").CreateLogger();
+        var log = new LoggerConfiguration()
+            .WriteTo
+            .File(new JsonFormatter(), Application.dataPath + "/AppLog.json")
+            .CreateLogger();
+
         Log.Logger = log;
     }
 
--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Serilog;
 
 public class MainCamera : MonoBehaviour
 {
     void Start()
     {
         Screen.orientation = ScreenOrientation.Portrait;
-        Debug.Log(">>>>>>>>>>>>>>> MainCamera");
-        Debug.Log("_______________ Start()");
+        Log.Information("Dividing {A} by {B}", 1.2f, 2.0f);
+        Debug.Log("Dividing {A} by {B}");
+
     }
 }
