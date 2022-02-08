19e4ef8b730d13d8443dac9661281e5d35e38cb1
Elodie Royant <elodie.royant@lexiko.io>
2022-02-08 14:07:07 +0100

    barriers as tiles too

--- Assets/Scripts/Frameworks/Controllers/GridController.cs
+++ Assets/Scripts/Frameworks/Controllers/GridController.cs
@@ -13,10 +13,10 @@ public class GridController : MonoBehaviour
 
     public Vector3 MainCharacterInitPosition;
     public EnumCharacterDirection MainCharacterInitDirection;
-    public Vector3 GridSize;
-    public GameObject LightTile;
-    public GameObject DarkTile;
-    public float Gutter;
+    //public Vector3 GridSize;
+    //public GameObject LightTile;
+    //public GameObject DarkTile;
+    //public float Gutter;
 
 
     [Inject]
@@ -27,32 +27,32 @@ public class GridController : MonoBehaviour
 
     void Start()
     {
-        DrawGrid();
+        //DrawGrid();
         _container.Resolve<CreateCharacter>().Execute(new CreateCharacterCommand(EnumCharacterType.BLACK_BIRD, MainCharacterInitDirection, (MainCharacterInitPosition[0], MainCharacterInitPosition[1], MainCharacterInitPosition[2]), Speed.INIT_SPEED));
     }
 
-    void DrawGrid()
-    {
-        for (int z = 0; z < GridSize.z; z++)
-        {
-            for (int y = 0; y < GridSize.y; y++)
-            {
-                for (int x = 0; x < GridSize.x; x++)
-                {
-                    if (x % 2 == 0 && z % 2 == 0 || x % 2 == 1 && z % 2 == 1)
-                    {
-                        GameObject _currentGo = Instantiate(DarkTile, new Vector3(x * (1 + Gutter), y * (1 + Gutter), z * (1 + Gutter)), Quaternion.identity);
-                        _currentGo.transform.parent = transform;
-                    }
-                    else
-                    {
-                        GameObject _currentGo = Instantiate(LightTile, new Vector3(x * (1 + Gutter), y * (1 + Gutter), z * (1 + Gutter)), Quaternion.identity);
-                        _currentGo.transform.parent = transform;
-                    }
-                }
-            }
-        }
-    }
+    //void DrawGrid()
+    //{
+    //    for (int z = 0; z < GridSize.z; z++)
+    //    {
+    //        for (int y = 0; y < GridSize.y; y++)
+    //        {
+    //            for (int x = 0; x < GridSize.x; x++)
+    //            {
+    //                if (x % 2 == 0 && z % 2 == 0 || x % 2 == 1 && z % 2 == 1)
+    //                {
+    //                    GameObject _currentGo = Instantiate(DarkTile, new Vector3(x * (1 + Gutter), y * (1 + Gutter), z * (1 + Gutter)), Quaternion.identity);
+    //                    _currentGo.transform.parent = transform;
+    //                }
+    //                else
+    //                {
+    //                    GameObject _currentGo = Instantiate(LightTile, new Vector3(x * (1 + Gutter), y * (1 + Gutter), z * (1 + Gutter)), Quaternion.identity);
+    //                    _currentGo.transform.parent = transform;
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
 
 }
 
