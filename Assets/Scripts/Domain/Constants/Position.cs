0ae7bad67464ce36f98a574e96c295699c17f05c
Elodie Royant <elodie.royant@lexiko.io>
2022-02-18 17:28:21 +0100

    cute bird is moving

--- Assets/Scripts/Domain/Constants/Position.cs
+++ Assets/Scripts/Domain/Constants/Position.cs
@@ -3,10 +3,10 @@
     public static class Position
     {
         public const float INIT_Y = 0.5f;
-        public const float X_MIN = 0.5f;
-        public const float X_MAX = 7.5f;
-        public const float Z_MIN = 0.5f;
-        public const float Z_MAX = 10.5f;
+        public const float X_MIN = -3.5f;
+        public const float X_MAX = 6.5f;
+        public const float Z_MIN = -1.5f;
+        public const float Z_MAX = 11.5f;
     }
 }
 
