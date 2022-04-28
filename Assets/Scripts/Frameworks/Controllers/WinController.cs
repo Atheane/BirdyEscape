f60133e3875a0ee63af92ca0790ac4b0e8b718ec
Elodie Royant <eroyant@gmail.com>
2022-04-28 11:43:24 +0200

    feat(popupwin) done

--- Assets/Scripts/Frameworks/Controllers/WinController.cs
+++ Assets/Scripts/Frameworks/Controllers/WinController.cs
@@ -5,16 +5,29 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 using UniMediator;
+using Zenject;
 using Adapters.Unimediatr;
 using Domain.DomainEvents;
+using Domain.Entities;
+using Usecases;
+using Usecases.Commands;
+
 
 public enum WinState { SHOWN, HIDDEN };
 
 public class WinController : MonoBehaviour, IMulticastMessageHandler<DomainEventNotification<GameLevelCompleted>>
 {
     public WinState _state;
+    public IGameEntity _gameEntity;
 
-    // Start is called before the first frame update
+    private DiContainer _container;
+
+    [Inject]
+    public void Construct(DiContainer container)
+    {
+        _container = container;
+    }
+
     private void Start()
     {
         _state = WinState.HIDDEN;
@@ -27,12 +40,30 @@ public class WinController : MonoBehaviour, IMulticastMessageHandler<DomainEvent
         Debug.Log("______" + notification._domainEvent._label + "_____handled");
         _state = WinState.SHOWN;
         gameObject.SetActive(true);
-        var energy = notification._domainEvent._props._energy.Value;
+        _gameEntity = notification._domainEvent._props;
+        var energy = _gameEntity._energy.Value;
         var slider = gameObject.GetComponentInChildren<Slider>();
-        slider.value = energy / 100;
+        slider.value = energy;
         var text = GameObject.FindWithTag("EnergyStatus").GetComponent<TextMeshProUGUI>();
         text.text = Mathf.RoundToInt(energy).ToString() + "/100";
-        //SceneManager.LoadScene("Level" + nextLevelNumber, LoadSceneMode.Single);
+    }
+
+    public void OnClickButtonHome()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void OnClickButtonRestart()
+    {
+        var levelNumber = _gameEntity._currentLevel._number.ToString();
+        //_container.Resolve<RestartLevel>().Execute(new RestartLevelCommand(_gameEntity._currentLevel._id));
+        SceneManager.LoadScene("Level" + levelNumber, LoadSceneMode.Single);
+    }
+
+    public void OnClickButtonNext()
+    {
+        var nextLevelNumber = (_gameEntity._currentLevel._number + 1).ToString();
+        SceneManager.LoadScene("Level" + nextLevelNumber, LoadSceneMode.Single);
     }
 
 }
